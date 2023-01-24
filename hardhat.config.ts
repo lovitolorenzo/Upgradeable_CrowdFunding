9bd4caed23a35ac97c029de4a2a55cdecbf7dd3c
Lorenzo <lovitolorenzo@yahoo.it>
2023-01-24 15:45:20 +0100

    downloads dependencies for upgradeable contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-etherscan";
 
-const config: HardhatUserConfig = {
+require("dotenv").config();
+
+const { API_URL, RESERVE3_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
+module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      accounts: [RESERVE3_PRIVATE_KEY ? RESERVE3_PRIVATE_KEY : ""], //deployer
+      url: API_URL,
+      //blockGasLimit: 100000000000000,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
-
-export default config;
