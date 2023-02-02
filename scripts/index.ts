2f3e79bad7adfbcfd51dcb3a4f3eface0e5776d7
Lorenzo <lovitolorenzo@yahoo.it>
2023-02-02 21:02:18 +0100

    adds interactions with refunding smart contract function, not yet finished

--- scripts/index.ts
+++ scripts/index.ts
@@ -23,18 +23,20 @@ async function main() {
   // Get EVM's funded wallets
   const [deployer, acc1, acc2] = await ethers.getSigners();
 
+  const crowdEndDate = currentBlockTimestamp + 10;
+
   // Start the crowdfund
   const startCrowd = await CrowdFundingContract.connect(
     deployer
   ).startCrowdfunding(
     "Project X",
-    currentBlockTimestamp + 100,
+    crowdEndDate,
     ethers.utils.parseEther("1000")
   );
 
   const balanceBeforeFunding = await acc1.getBalance();
   console.log(
-    `Starting Funder balance: ${ethers.utils.formatEther(balanceBeforeFunding)}`
+    `Starting funder balance: ${ethers.utils.formatEther(balanceBeforeFunding)}`
   );
 
   // Fund the crowdfund
@@ -44,13 +46,67 @@ async function main() {
 
   const balanceAfterFunding = await acc1.getBalance();
   console.log(
-    `Starting Funder balance: ${ethers.utils.formatEther(balanceAfterFunding)}`
+    `After funding balance: ${ethers.utils.formatEther(balanceAfterFunding)}`
   );
 
-  // End the crowdfund
-  // Add code to end the crowdfund here
+  console.log(
+    balanceBeforeFunding > balanceAfterFunding
+      ? "Crowdfund has been funded successfully!"
+      : "Something went wrong in the funding process"
+  );
 
-  console.log("Crowdfund started and funded successfully!");
+  // Withdraw funds
+
+  const balanceBeforeRefunding = await acc1.getBalance();
+  console.log(
+    `Before refunding balance: ${ethers.utils.formatEther(
+      balanceBeforeRefunding
+    )}`
+  );
+
+  // Check withdraw existence
+
+  const crowdExists =
+    (await CrowdFundingContract.connect(acc1).AllCrowds(deployer.address)) &&
+    (await CrowdFundingContract.connect(acc1).AllCrowds(deployer.address));
+
+  if (!crowdExists) {
+    console.log("No Crowd Found for refunding");
+    return;
+  }
+
+  // Check Crowd Funding is ended
+
+  const crowdEnded =
+    currentBlockTimestamp > Number(crowdExists.date) ? true : false;
+
+  if (!crowdEnded) {
+    console.log("Crowd not yet ended. Waiting for it");
+    // Wait the Crowd Funding to end
+    const waitToEnd = await waitForTimestamp(crowdEndDate);
+    waitToEnd && console.log("Crowd fund ended, proceeding to the refunding");
+  }
+
+  // Calling the smart contract function to receive back the funds
+
+  const refundCall = await CrowdFundingContract.connect(acc1).refunding(
+    deployer.address
+  );
+
+  const txRefund = await refundCall.wait();
+
+  const balanceAfterRefunding = await acc1.getBalance();
+  console.log(
+    `After refunding balance: ${ethers.utils.formatEther(
+      balanceAfterRefunding
+    )}`
+  );
+
+  txRefund &&
+    console.log(
+      balanceAfterRefunding > balanceBeforeRefunding &&
+        "Refunding correctly done"
+    );
 }
 
 main()
@@ -59,3 +115,16 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+async function waitForTimestamp(timestamp: number) {
+  let latest = (await ethers.provider.getBlock("latest")).timestamp;
+  while (latest <= timestamp) {
+    const currentTimestamp = (await ethers.provider.getBlock("latest"))
+      .timestamp;
+    latest != currentTimestamp &&
+      console.log(`Current timestamp: ${currentTimestamp}`);
+
+    latest = currentTimestamp;
+  }
+  if (latest > timestamp) return true;
+}
